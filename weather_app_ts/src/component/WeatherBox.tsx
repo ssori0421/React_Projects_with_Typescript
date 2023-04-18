0f64b6e7ac3d68e4f1bf5f57fe31460fb43da5fa
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-18 23:50:15 +0900

    wheatherBox 타입 적용, 조건부 렌더링 수정

--- weather_app_ts/src/component/WeatherBox.tsx
+++ weather_app_ts/src/component/WeatherBox.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { IWeather } from '../types/weather';
 
-const WeatherBox = ({ weather }: { weather: any }) => {
+const WeatherBox = ({ weather }: { weather: IWeather }) => {
   return (
     <div className='weatherWrap'>
-      <div>{weather && weather.name}</div>
+      <div>{weather.name}</div>
       <h2>
-        {weather && weather.main.temp}℃ /
-        {parseInt(weather && weather.main.temp / 1.8)}℉
+        {weather.main.temp}℃ /{weather.main.temp / 1.8}℉
       </h2>
-      <h3>{weather && weather.weather[0].description}</h3>
+      <h3>{weather.weather[0].description}</h3>
     </div>
   );
 };
