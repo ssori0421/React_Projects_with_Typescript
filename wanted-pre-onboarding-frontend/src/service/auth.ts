e5735a8e2497fb6c6ccff1c3ffe32ea4ce0e3868
ssori <115159126+ssori0421@users.noreply.github.com>
2023-04-30 20:59:51 +0900

    auth api 관련 type 지정

--- wanted-pre-onboarding-frontend/src/service/auth.ts
+++ wanted-pre-onboarding-frontend/src/service/auth.ts
@@ -1,12 +1,16 @@
+import { ISignInRes, ISignReq } from '../types/auth';
 import { setAccessToken } from '../util/localstorage';
 import { instance } from './axios';
 
-const postSignUp = async (body:any) => {
+const postSignUp = async (body: ISignReq) => {
   await instance.post('/auth/signup', body);
 };
 
-const postSignIn = async (body:any) => {
-  const { data } = await instance.post('/auth/signin', body);
+const postSignIn = async (body: ISignReq) => {
+  const { data }: { data: ISignInRes } = await instance.post(
+    '/auth/signin',
+    body
+  );
   setAccessToken(data.access_token);
 };
 
